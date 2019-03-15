02191ac02b0752b9a6cddf2578aca6944a09ce20
JYOTHI <JYOTHI@JYOTHI-PC>
2019-03-15 10:17:49 +0530

    Commit

---
+++ Project_1/src/com/Test/java/Class_1_Add.java
@@ -0,0 +1,13 @@
+package com.Test.java;
+
+public class Class_1_Add {
+
+	public static void main(String[] args) 
+	{
+		int a=10,b=36,c;
+		c=a+b;
+		System.out.println(c);
+
+	}
+
+}
---
+++ Project_1/src/com/Test/java/Class_2_Sub.java
@@ -0,0 +1,13 @@
+package com.Test.java;
+
+public class Class_2_Sub {
+
+	public static void main(String[] args) 
+	{
+		int a=10,b=36,c;
+		c=b-a;
+		System.out.println(c);
+
+	}
+
+}
